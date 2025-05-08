7e31a057259509425ab0e7ec460475cd44d6d2e0
helenamMm <helenamm.001@gmail.com>
2025-05-07 21:27:01 -0600

    probando con el otro TURN server ianopuedo si no jala luego le sigo intentando alv

--- src/hooks/useVideoCall.js
+++ src/hooks/useVideoCall.js
@@ -16,10 +16,10 @@ const servers = {
       urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'] 
     },
     {
-      urls: 'turn:openrelay.metered.ca:80',
-      username: 'openrelayproject',
-      credential: 'openrelayproject'
-    },
+      urls: 'turn:numb.viagenie.ca',
+      credential: 'webrtc',
+      username: 'webrtc@live.com'
+    }
   ],
   iceCandidatePoolSize: 10,
 };
